cd940dbe479dc7341b57b28d8ddfacdd2545f8c1
tiantang88 <466684860@qq.com>
2018-08-31 17:32:09 +0800

    搞笑娱乐提交

---
+++ core/git.version.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* iCMS - i Content Management System
+* Copyright (c) 2007-2018 iCMSdev.com. All rights reserved.
+*
+* @author icmsdev <master@icmsdev.com>
+* @site https://www.icmsdev.com
+* @licence https://www.icmsdev.com/LICENSE.html
+*/
+define('GIT_COMMIT', 'ee91033af8e652a371222edf4e6bcb35ac8462bc');
+define('GIT_AUTHOR', 'icmsdev');
+define('GIT_EMAIL', 'master@icmsdev.com');
+define('GIT_TIME', '1535364057');
